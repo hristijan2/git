2da96e5b6dffef2118c623bf69e6b89823ac32a8
hristijan2 <83391779+hristijan2@users.noreply.github.com>
2021-05-30 16:25:08 +0200

    Add files via upload

---
+++ UpperAndLowerCase.java
@@ -0,0 +1,13 @@
+package DayEight;
+
+public class UpperAndLowerCase {
+
+	public static void main(String[] args) {
+		String f1 = "i love playing football";
+		String f2 = "I LOVE PLAYING FOTBALL";
+		
+		System.out.println(f1.toUpperCase());
+		System.out.println(f2.toLowerCase());
+	}
+
+}
